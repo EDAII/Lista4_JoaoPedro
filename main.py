c997e8f46f6ca9f91ef28f148e8a17c2d56160cb
João Soares <jpcirqueira81@gmail.com>
2019-05-10 23:01:46 -0300

    atualizado

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ import pandas as pd
 
 def print_menu():
     print(30 * "-" , "MENU" , 30 * "-")
-    print("1. Grafico  heap sort ")
-    print("2. Gera csv com relatório dos tempos de ordenação dos algoritmos")
-    print("3. Sair")
+    print("1. visualização do heap sort")
+    print("2. Grafico heap sort ")
+    print("3. Grafico radix sort ")
+    print("4. Gera csv com relatório dos tempos de ordenação dos algoritmos")
+    print("5. Sair")
     print(67 * "-")
 
 
@@ -27,6 +29,70 @@ def seleciona_algoritmo(algoritmo, lista, tempo_ordenacao, nome_algoritmo):
         tempo_cronometragem = fim_cronometragem - inicio_cronometagem
         tempo_ordenacao[nome_algoritmo][i] = tempo_cronometragem
         insere_randomico(lista, 100)    
+
+def iteracoes_visual():
+    print("Opcao 3 foi escolhida")
+    print("[16, 23, 12, 3, 19, 7, 31]")
+    print("\n")
+    print("neste passo distribui os valores do vetor para uma arvore")
+    print("\n")
+    print("           16          ")
+    print("         /    \        ")
+    print("      23        12     ")
+    print("    /    \     /   \   ")
+    print("  3       19  7     31 ")
+    print("\n")
+    print("neste passo foi passado os maiores valores para a 'raiz' da arvores")
+    print("\n")
+    print("           23          ")
+    print("         /    \        ")
+    print("      19        12     ")
+    print("    /    \     /   \   ")
+    print("  3      16   7     31 ")
+    print("\n")
+    print("neste passo troca o elemento da raiz por o ultimo da lista e ignora o novo ultimo elemento daqui para frente")
+    print("\n")
+    print("           7           ")
+    print("         /    \        ")
+    print("      19        12     ")
+    print("    /    \     /   \   ")
+    print("  3      16   23    31 ")
+    print("\n")
+    print("neste passo foi passado os maiores valores para a 'raiz' da arvores")
+    print("\n")
+    print("           19          ")
+    print("         /    \        ")
+    print("      16        12     ")
+    print("    /    \     /   \   ")
+    print("  3       7   23    31 ")
+    print("\n")
+    print("neste passo troca o elemento da raiz por o ultimo da lista e ignora o novo ultimo elemento daqui para frente")
+    print("\n")
+    print("           7           ")
+    print("         /    \        ")
+    print("      16        12     ")
+    print("    /    \     /   \   ")
+    print("  3       19   23    31")
+    print("\n")
+    print("neste passo foi passado os maiores valores para a 'raiz' da arvores")
+    print("\n")
+    print("           16          ")
+    print("         /    \        ")
+    print("      7        12      ")
+    print("    /   \     /   \    ")
+    print("  3     19  23     31  ")
+    print("\n")
+    print("neste passo troca o elemento da raiz por o ultimo da lista e ignora o novo ultimo elemento daqui para frente")
+    print("\n")
+    print("           3          ")
+    print("         /    \        ")
+    print("      7        12      ")
+    print("    /   \     /   \    ")
+    print("  16     19  23     31  ")
+    print("\n")
+    print("[7, 3, 12, 17, 19, 23, 31]")
+    print("\n")
+    print("Vetor ordenado")
 
 if __name__ == '__main__':
     loop=True
@@ -44,22 +110,31 @@ if __name__ == '__main__':
 
     while loop:
         print_menu()
-        choice = input("Entre sua opcao [1-3]: ")
-        if choice=='1':     
+        choice = input("Entre sua opcao [1-5]: ")
+        if choice=='1':
             print("Opcao 1 foi escolhida")
+            iteracoes_visual()
+        elif choice=='2':     
+            print("Opcao 2 foi escolhida")
             lista = random.sample(range(0,tamanho_vetor * 2), tamanho_vetor)
             seleciona_algoritmo(heap_sort, lista, tempo_ordenacao_python, 'heap_sort')
             seleciona_algoritmo(heap_sort_interativo, lista, tempo_ordenacao_python, 'heap_sort_iterativo')
+            # seleciona_algoritmo(radixSort, lista, tempo_ordenacao_python, 'radix_sort')
+            plot_grafico(tempo_ordenacao_python)
+        elif choice=='3':     
+            print("Opcao 3 foi escolhida")
+            lista = random.sample(range(0,tamanho_vetor * 2), tamanho_vetor)
             seleciona_algoritmo(radixSort, lista, tempo_ordenacao_python, 'radix_sort')
             plot_grafico(tempo_ordenacao_python)
-        elif choice=='2':
-            print("Opcao 1 foi escolhida")
+        elif choice=='4':
+            print("Opcao 4 foi escolhida")
             lista = random.sample(range(0,tamanho_vetor * 2), tamanho_vetor)
             seleciona_algoritmo(heap_sort, lista, tempo_ordenacao_python, 'heap_sort')
             seleciona_algoritmo(heap_sort_interativo, lista, tempo_ordenacao_python, 'heap_sort_iterativo')
+            seleciona_algoritmo(heap_sort_interativo, lista, tempo_ordenacao_python, 'radix_sort')
             gera_csv_resultado(tempo_ordenacao_python)
-        elif choice=='3':
-            print("Opcao 3 foi escolhida")
+        elif choice=='5':
+            print("Opcao 5 foi escolhida")
             print('Saindo....')
             loop=False
         else:
